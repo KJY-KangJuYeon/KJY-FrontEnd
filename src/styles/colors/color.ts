9c18e51833fcdfd755cc1c8223f40f768284e6d2
juwon <juwon2298@naver.com>
2023-12-01 19:05:39 +0900

    feat: Color Palette 컬러 등록
    GrayScale, Primary, Secondary 등록

---
+++ src/styles/colors/color.ts
@@ -0,0 +1,81 @@
+export const black = '#000000';
+export const black100 = black;
+export const Dark_BG = '#121212';
+
+export const GrayScale50 = '#FAFAFA';
+export const GrayScale100 = '#E0E0E0';
+export const GrayScale200 = '#C7C7C7';
+export const GrayScale300 = '#ADADAD';
+export const GrayScale400 = '#949494';
+export const GrayScale500 = '#7A7A7A';
+export const GrayScale600 = '#616161';
+export const GrayScale700 = '#474747';
+export const GrayScale800 = '#2E2E2E';
+export const GrayScale900 = '#141414';
+
+export const GrayScale = {
+  50: GrayScale50,
+  100: GrayScale100,
+  200: GrayScale200,
+  300: GrayScale300,
+  400: GrayScale400,
+  500: GrayScale500,
+  600: GrayScale600,
+  700: GrayScale700,
+  800: GrayScale800,
+  900: GrayScale900,
+};
+
+export const Primary50 = '#E8EAF6';
+export const Primary100 = '#C5CBE9';
+export const Primary200 = '#9FA9DA';
+export const Primary300 = '#7885CC';
+export const Primary400 = '#5B6CC1';
+export const Primary500 = '#3F53B5';
+export const Primary600 = '#3943AB';
+export const Primary700 = '#2E379E';
+export const Primary800 = '#262F97';
+export const Primary900 = '#1B237D';
+
+export const Primary = {
+  50: Primary50,
+  100: Primary100,
+  200: Primary200,
+  300: Primary300,
+  400: Primary400,
+  500: Primary500,
+  600: Primary600,
+  700: Primary700,
+  800: Primary800,
+  900: Primary900,
+};
+
+export const Secondary50 = '#F1EDF8';
+export const Secondary100 = '#D3C8EA';
+export const Secondary200 = '#B6A3DC';
+export const Secondary300 = '#9168CA';
+export const Secondary400 = '#7953C6';
+export const Secondary500 = '#663DB8';
+export const Secondary600 = '#5E34B2';
+export const Secondary700 = '#552BAA';
+export const Secondary800 = '#4729A3';
+export const Secondary900 = '#301B98';
+
+export const Secondary = {
+  50: Secondary50,
+  100: Secondary100,
+  200: Secondary200,
+  300: Secondary300,
+  400: Secondary400,
+  500: Secondary500,
+  600: Secondary600,
+  700: Secondary700,
+  800: Secondary800,
+  900: Secondary900,
+};
+
+export const colors = {
+  GrayScale,
+  Primary,
+  Secondary
+}
